e4b8a9db4de8bc49cd93c80dbc4ea3e05e35489b
lwazi Jakuja <visionaryvj@gmail.com>
2025-10-09 16:09:44 +0200

    Second commit

--- app/Livewire/Chat/Chat.php
+++ app/Livewire/Chat/Chat.php
@@ -3,9 +3,22 @@
 namespace App\Livewire\Chat;
 
 use Livewire\Component;
+use App\Models\Conversation;
 
 class Chat extends Component
 {
+
+    // public $selectedConversation;
+    public $query;
+    public $selectedConversation;
+
+    public function mount($query) // The route parameter is injected here
+{
+    // The $query variable now correctly holds the ID from the URL
+    $this->query = $query;
+    $this->selectedConversation = Conversation::findOrFail($this->query);
+}
+
     public function render()
     {
         return view('livewire.chat.chat');
--- app/Livewire/Users.php
+++ app/Livewire/Users.php
@@ -3,6 +3,7 @@
 namespace App\Livewire;
 
 use Livewire\Component;
+use App\models\Conversation;
 
 class Users extends Component
 {
@@ -12,9 +13,38 @@ class Users extends Component
      * The mount method is like a constructor. It runs when the
      * component is first initialized.
      */
+    public function message($userId)
+    {
+        $authenticatedUserId = auth()->id();
+
+        $existingConversation = Conversation::where( function ($query) use ($authenticatedUserId, $userId) {
+            $query->where('sender_id', $authenticatedUserId)
+            ->where('receiver_id', $userId);
+
+        })->orWhere( function ($query) use ($authenticatedUserId, $userId) {
+            $query->where( function ($query) use ($authenticatedUserId, $userId) {
+                $query->where('sender_id', $userId)
+                ->where('receiver_id', $authenticatedUserId);
+            });
+        })->first();
+        // dd($existingConversation);
+
+        if($existingConversation)
+        {
+            return redirect()->route('chat.chat', ['query' => $existingConversation->id]);
+        }
+
+        $createdConversation = Conversation::create([
+            'sender_id' => $authenticatedUserId,
+            'receiver_id' => $userId,
+        ]);
+
+        return redirect()->route('chat.chat', ['query' => $createdConversation->id]);
+    }
+
     public function mount($users)
     {
-        $this->users = $users;
+        $this->users = $users->except(auth()->id());
     }
     
     public function render()
